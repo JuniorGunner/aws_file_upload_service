1b9136e27957f2416fafc34e54f07043bbf8a168
JuniorGunner <csf.junior90@gmail.com>
2023-09-28 20:44:34 -0300

    auth endpoint, access token generation

--- lambda_functions/auth/lambda_function.py
+++ lambda_functions/auth/lambda_function.py
@@ -1,15 +1,24 @@
+import os
 import jwt
 import datetime
-import http
+from http import HTTPStatus
 
-SECRET_KEY = "YOUR_SECRET_KEY"  # Store in AWS Secrets Manager in production
+SECRET_KEY = os.getenv('SECRET_KEY') # NOT GETTING THE KEY EXPORTED - WHY?
+
 
 def lambda_handler(event, context):
     try:
-        token = jwt.encode({"exp": datetime.datetime.utcnow() + datetime.timedelta(days=1)}, SECRET_KEY, algorithm="HS256")
+        print(f"{os.environ=}")
+        print(f"{SECRET_KEY=}")
+
+        token = jwt.encode(
+            {"exp": datetime.datetime.utcnow() + datetime.timedelta(days=1)},
+            SECRET_KEY,
+            algorithm="HS256"
+        )
         return {
-            "statusCode": http.HTTPStatus.OK,
+            "statusCode": HTTPStatus.OK,
             "body": {"token": token}
         }
     except Exception as e:
-        return {"statusCode": http.HTTPStatus.INTERNAL_SERVER_ERROR, "body": str(e)}
+        return {"statusCode": HTTPStatus.INTERNAL_SERVER_ERROR, "body": str(e)}
